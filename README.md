c356bb6712d33cd1ceba0206a979d79de86105c4
mluker <luker.matt@gmail.com>
2023-08-05 15:12:57 -0500

    refactor to a working app with a simple docker-compose

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# task-api
+# Task api
 A sample app to demonstrate a simple API in written in Python leveraging a database and an Object-Relational Mapping (ORM) framework.
 
 Technologies used in this project include:
@@ -9,137 +9,18 @@ Technologies used in this project include:
 * SQLAlchemy
 * Docker
 
-## API endpoints
-
-### Single task - GET
-
-``` text
-/tasks/{task-id}
+## Run application
+```bash
+cd task-list
+docker-compose up --build
 ```
 
-Returns:
-
-``` json
-{
-    "id": 42,
-    "title": "Get a coffee",
-    "description": "Get a coffee, make sure it's a really good one!",
-    "due_date": 2024-07-02,
-    "completed": false,
-    "created_at": 2023-07-01,
-    "created_by": "Jane Doe"
-}
-```
-
-### All tasks - GET
-
-``` text
-/tasks
-```
-
-Returns:
-
-``` json
-{
-    "tasks": [
-        {
-            "id": 42,
-            "title": "Get a coffee",
-            "description": "Get a coffee, make sure it's a really good one!",
-            "due_date": 2024-07-01,
-            "completed": false,
-            "created_at": 2023-07-01,
-            "created_by": "Jane Doe"
-        },
-        {
-            "id": 82,
-            "title": "Get a new car",
-            "description": "Make sure it's a 4x4",
-            "due_date": 2025-12-01,
-            "completed": false,
-            "created_at": 2023-04-10,
-            "created_by": "John Doe"
-        }
-    ]
-}
-```
-
-### Create new task - POST
-
-``` text
-/tasks
-```
-
-POST body:
-
-``` json
-{
-    "title": "Go the beach",
-    "description": "Make sure it's at least 90 degrees",
-    "due_date": "2025-06-01"
-}
-```
-
-### Update task - PUT
-
-``` text
-/tasks/{task_id}
-```
-
-PUT body:
-
-``` json
-{
-    "title": "Go the beach",
-    "description": "Make sure it's at least 90 degrees",
-    "due_date": "2025-06-01",
-    "completed": true
-}
-```
-
-### Delete task - DELETE
-
-``` text
-/tasks/{task_id}
-```
-
-## DB Schema
-
-TASK table:
-
-``` database
-- id (primary key)
-- title (string)
-- description (string)
-- due_date (date)
-- completed (boolean)
-- created_at (timestamp)
-- created_by (id of user who created task)
-```
-
-USER table:
-
-``` text
-- id (primary key)
-- first_name (string)
-- last_name (string)
-```
-
-
-
-## Start UVICORN SERVER LOCALLY
-
-```
+## Start uvicorn server
+```bash
 cd task-list
 uvicorn api.main:app --reload
 ```
 
-## Start Build Application as Container 
-
-```
-cd task-list
-
-chmod u+x build.sh
-
-./build.sh
-```
+## Also see:
+* [API documentation](api.md)
+* [DB schema](db.md)
--- task-list/api/main.py
+++ task-list/api/main.py
@@ -1,75 +1,88 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+import datetime
+from faker import Faker
+import schemas, models
 
-class ModelGetTask(BaseModel):
-    id:int
-    title:str
-    description:str
-    due_date:str
-    completed:bool
-    created_at:str
-    created_by:str
+from db import get_db, engine, Base
 
-class ModelPostTask(BaseModel):
-    title:str
-    description:str
-    due_date:str
-    completed:bool
-    created_by:str
-
-class ModelGetUser(BaseModel):
-    id:int
-    name:str
-    email:str
-    password:str
-
-class ModelPostUser(BaseModel):
-    name:str
-    email:str
-    password:str
-
+# Initialize FastAPI app
 app = FastAPI()
 
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return "Task API is running"
 
+# Get all tasks
 @app.get("/tasks")
-async def tasks()->list[ModelGetTask]:
-    return [
-        ModelGetTask(id=1, title="title", description="description", due_date="2021-01-01", completed=False, created_at="2021-01-01", created_by="user"),
-        ModelGetTask(id=2, title="title", description="description", due_date="2021-01-01", completed=False, created_at="2021-01-01", created_by="user"),
-        ModelGetTask(id=3, title="title", description="description", due_date="2021-01-01", completed=False, created_at="2021-01-01", created_by="user"),
-        ModelGetTask(id=4, title="title", description="description", due_date="2021-01-01", completed=False, created_at="2021-01-01", created_by="user")
-    ]
+def get_tasks(db: Session = Depends(get_db), search: str = ''):
+    tasks = db.query(models.TaskEntity).filter(or_(
+        models.TaskEntity.title.contains(search),
+        models.TaskEntity.description.contains(search))).all()
+    return {"status": "success", "task": tasks}
 
+# Get single task by id
 @app.get("/tasks/{task_id}")
-async def task(task_id:int)->ModelGetTask:
-    return ModelGetTask(id=task_id, title="title", description="description", due_date="2021-01-01", completed=False, created_at="2021-01-01", created_by="user")
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.TaskEntity).filter(models.TaskEntity.id == task_id).first()
+    if not task: raise HTTPException(status_code=404, detail="Task not found")
+    return {"status": "success", "task": task}
 
+# Create task
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: schemas.TaskSchema, db: Session = Depends(get_db)):
+    new_task = models.TaskEntity(**task.dict())
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return {"status": "success", "task": new_task}
+
+# Update task
+@app.put("/tasks/{task_id}", status_code=status.HTTP_201_CREATED)
+def update_task(task_id: int, task: schemas.TaskSchema, db: Session = Depends(get_db)):
+    existing_task = db.query(models.TaskEntity).filter(models.TaskEntity.id == task_id).first()
+    if not existing_task: raise HTTPException(status_code=404, detail="Task not found")
+
+    # map the updated fields to the task object
+    for k, v in task.dict().items(): setattr(existing_task, k, v)
+
+    db.commit()
+    db.refresh(existing_task)
+    return {"status": "success", "task": existing_task}
+
+# Delete task
 @app.delete("/tasks/{task_id}")
-async def task(task_id:int):
-    return {"message": "delete_task", "param" : task_id}
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.TaskEntity).filter(models.TaskEntity.id == task_id).first()
+    if not task: raise HTTPException(status_code=404, detail="Task not found")
 
-@app.post("/tasks", status_code=201)
-async def tasks(model:ModelPostTask) -> ModelPostTask:
-    return ModelPostTask(title=model.title, description=model.description, due_date=model.due_date, completed=model.completed, created_by=model.created_by)
+    db.delete(task)
+    db.commit()
+    return {"status": "success", "task": task}
 
-@app.put("/tasks")
-async def tasks():
-    return {"message": "put_task"}
 
-@app.get("/users")
-async def users()->list[ModelGetUser]:
-    return [
-        ModelGetUser(id=1, name="name", email="email", password="password"),
-        ModelGetUser(id=2, name="name", email="email", password="password"),
-    ]
+################################
+## FOR TESTING PURPOSES ONLY ##
+################################
 
-@app.get("/users/{user_id}")
-async def user(user_id:int)->ModelGetUser:
-    return ModelGetUser(id=user_id, name="name", email="email", password="password")
+# seed database with tasks
+@app.get("/tasks/seed/", status_code=status.HTTP_201_CREATED)
+def seed_task(db: Session = Depends(get_db)):
+    for i in range(1, 200):
+        fake = Faker()
+        t =  schemas.TaskSchema(title=fake.text(20), description=fake.text(), completed=False, due_date=datetime.datetime.now() , created_by=fake.name())
+        new_task = models.TaskEntity(**t.dict())
+        db.add(new_task)
+        db.commit()
+    return {"status": "seeded tasks"}
 
-@app.post("/users", status_code=201)
-async def user(model):
-    return {"message": "post_user", "object" : model}
+# clear database
+@app.delete("/tasks/delete/", status_code=status.HTTP_200_OK)
+def seed_task(db: Session = Depends(get_db)):
+    db.query(models.TaskEntity).delete()
+    db.commit()
+    return {"status": "deleted all tasks"}
