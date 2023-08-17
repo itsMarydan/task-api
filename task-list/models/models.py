9fe9268fe03cd925e8d6251aeb6bd9b273cf1649
mluker <luker.matt@gmail.com>
2023-08-17 14:03:47 -0500

    clean up

--- task-list/models/models.py
+++ task-list/models/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, String, Boolean, TIMESTAMP, Column, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-#from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 from typing import List
 
